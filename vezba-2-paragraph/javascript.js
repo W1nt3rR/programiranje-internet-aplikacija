83ad64b03dfc5fb757600702ec63fdac3477e382
Ramiz Sabovic <frostywinterr@gmail.com>
2022-10-13 14:57:46 +0200

    changeImage button

--- vezba-2-paragraph/javascript.js
+++ vezba-2-paragraph/javascript.js
@@ -11,5 +11,13 @@ const changeColor = () => {
     }, 1000)
 }
 
+const changeImage = () => {
+    const img = document.getElementById("wallpaper");
+    img.src = "images/wallhaven-2.jpg"
+    setTimeout(() => {
+        img.src = "images/wallhaven-1.jpg"
+    }, 1000)
+}
+
 const par = document.getElementById("par1");
 par.style.color = "orange";
