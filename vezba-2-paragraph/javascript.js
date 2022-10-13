e91664123cd4d6f43d25ddcb9a128acfeb7b839a
Ramiz Sabovic <frostywinterr@gmail.com>
2022-10-13 14:51:45 +0200

    Change color button

--- vezba-2-paragraph/javascript.js
+++ vezba-2-paragraph/javascript.js
@@ -3,6 +3,13 @@ const alertFn = () => {
     alert("Welcome to my world!");
 }
 
-const par = document.getElementById("par1");
+const changeColor = () => {
+    const par = document.getElementById("par1");
+    par.style.color = "red";
+    setTimeout(() => {
+        par.style.color = "orange";
+    }, 1000)
+}
 
+const par = document.getElementById("par1");
 par.style.color = "orange";
