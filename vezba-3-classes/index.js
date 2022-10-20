e6fda1f7eac6e142f5f2dc38531df5b25af43aba
Ramiz Sabovic <frostywinterr@gmail.com>
2022-10-20 13:25:05 +0200

    Age method

--- vezba-3-classes/index.js
+++ vezba-3-classes/index.js
@@ -3,4 +3,13 @@ class Car {
         this.name = name;
         this.year = year;
     }
+
+    age() {
+        let date = new Date();
+        return date.getFullYear() - this.year;
+    }
 }
+
+let myCar = new Car ("VW Passat B5.5", "2003");
+
+document.getElementById("demo").innerHTML = `My car is ${myCar.age()} years old.`
